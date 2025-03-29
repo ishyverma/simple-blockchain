f99c5e32593240d8b0e820b24e7b0d4eae9eb138
ishyverma <ishyamver@gmail.com>
2025-03-29 17:15:26 +0530

    basic mining done now race between all the miners to do pow

--- apps/ws/index.ts
+++ apps/ws/index.ts
@@ -4,9 +4,11 @@ import WebSocket, { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 })
 
-let id = 1;
+let num = 1;
 
 wss.on("connection", ws => {
+    console.log("connected")
+    ws.send(JSON.stringify({type:"connected"}))
     ws.on("message", async data => {
         const message = JSON.parse(data.toString())
         switch (message.type) {
@@ -14,9 +16,10 @@ wss.on("connection", ws => {
                 const from = message.payload.from // Id of the user who is sending money
                 const to = message.payload.to  // Id of the user who is recieving money
                 const amount = message.payload.amount
-                
+                console.log(from, to, amount)
                 // Add this 64 times 0 if there is no block in the blockchain for the first time and after that take the last hash from the blockchain
-                const previousHash = message.payload.previousHash
+                const previousHash = BLOCKCHAIN.length ? BLOCKCHAIN[BLOCKCHAIN.length - 1].currentHash : "0".repeat(64)
+                const prevId = BLOCKCHAIN.length ? BLOCKCHAIN[BLOCKCHAIN.length - 1].id + 1 : 1
 
                 try {
                     const promiseUser = await prisma.user.findUnique({
@@ -45,7 +48,8 @@ wss.on("connection", ws => {
                         from,
                         to,
                         amount,
-                        previousHash
+                        previousHash,
+                        id: prevId
                     }
                 })))
                 break;
@@ -57,7 +61,10 @@ wss.on("connection", ws => {
                     ws,
                     blockChainCopy: BLOCKCHAIN
                 })
-                ws.send("Ready to mine")
+                console.log(MINERS.slice(-1)[0].blockChainCopy)
+                ws.send(JSON.stringify({
+                    type: "ready to mine"
+                }))
                 break;
             }
             case "left_mine": {
@@ -77,25 +84,28 @@ wss.on("connection", ws => {
                 const from = message.payload.from
                 const to = message.payload.to
                 const amount = message.payload.amount
-                const previousHash = message.payload.prevHash
-
-                const { blockNumber, data, nonce, prevHash, currentHash } = proofOfWork(id, `${from}-${to} | Rs. ${amount}`, previousHash)
+                const previousHash = message.payload.previousHash
+                const id = message.payload.id
+                console.log('hiu there')
+                const { blockNumber, data, nonce, currentHash } = proofOfWork(id, `${from}-${to} | Rs. ${amount}`, previousHash)
+                console.log('hiu there2')
 
                 BLOCKCHAIN.push({
                     currentHash,
                     data,
                     id,
                     nonce: nonce,
-                    prevHash
+                    prevHash: previousHash
                 })
-
+                console.log("BLOCKCHAIN", BLOCKCHAIN)
                 // Sending user the data that he has mined
                 ws.send(JSON.stringify({
                     blockNumber,
                     data,
                     nonce,
-                    prevHash,
-                    currentHash
+                    prevHash: previousHash,
+                    currentHash,
+                    minerId: id
                 }))
 
                 break;
@@ -155,7 +165,7 @@ wss.on("connection", ws => {
                 BLOCKCHAIN.push({
                     currentHash: hash,
                     data,
-                    id, // Block Id
+                    id: num, // Block Id
                     nonce,
                     prevHash: BLOCKCHAIN.length ? BLOCKCHAIN[BLOCKCHAIN.length - 1].prevHash : "0".repeat(64)
                 })
@@ -165,14 +175,14 @@ wss.on("connection", ws => {
                         miner.blockChainCopy.push({
                             currentHash: hash,
                             data,
-                            id,
+                            id: num,
                             nonce,
                             prevHash: BLOCKCHAIN.length ? BLOCKCHAIN[BLOCKCHAIN.length - 1].prevHash : "0".repeat(64)
                         })
                     }
                 })
 
-                id += 1
+                num += 1
             }
         }
     })
